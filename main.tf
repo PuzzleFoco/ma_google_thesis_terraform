3611b33ee435fea6f18a341ed10b5f1a6771e240
Fabius Engel <Fabius.Engel@msg.group>
2020-06-06 22:41:29 +0200

    final commit for cluster

---
+++ main.tf
@@ -0,0 +1,89 @@
+provider "google" {
+    credentials = file("${path.module}/${var.credential_file_name}")
+    region  = var.location
+    zone    = "${var.location}-${var.zone}"
+}
+
+module "gke" {
+    source = "git::git@gitlab.hrz.tu-chemnitz.de:faeng--tu-chemnitz.de/terraform-google-kubernetes.git"
+
+    gke_cluster_name    = "googleexamplecluster"
+    location            = var.location
+    project             = var.project_id
+    min_master_version  = "1.15.9-gke.24"
+
+}
+
+provider "kubernetes" {
+  version                = "1.11.1"
+
+  load_config_file = false
+
+  host                   = module.gke.host
+  client_certificate     = base64decode(module.gke.client_certificate)
+  client_key             = base64decode(module.gke.client_key)
+  cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+  username = module.gke.cluster_username
+  password = module.gke.cluster_password
+}
+
+provider "helm" {
+    version = "~>1.0.0"
+
+    kubernetes {
+      host                   = module.gke.host
+
+      client_certificate     = base64decode(module.gke.client_certificate)
+      client_key             = base64decode(module.gke.client_key)
+      cluster_ca_certificate = base64decode(module.gke.cluster_ca_certificate)
+      load_config_file       = false
+      username = module.gke.cluster_username
+      password = module.gke.cluster_password
+  }
+}
+
+module "google_dns_zone" {
+    source  = "git::https://gitlab.hrz.tu-chemnitz.de/faeng--tu-chemnitz.de/terraform_google_dns_zone.git"
+
+    name            = "masterthesis-dns-zone"
+    dns_name        = "${var.root_domain}."
+    description     = "DNS Zone for masterthesis.online"
+    project         = var.project_id
+    public_ip_name  = "masterthesisonlineipaddress"
+    location        = var.location
+}
+
+module "nginx" {
+    source         = "git::https://gitlab.hrz.tu-chemnitz.de/faeng--tu-chemnitz.de/terraform_nginx_helm.git"
+
+    controller_service = {
+      "enabled"        : "true",
+      "loadBalancerIP" : module.google_dns_zone.ip_address,
+    }
+
+    annotations    = [
+        {
+          "annotation_key" : "kubernetes.io/ingress.global-static-ip-name",
+          "annotation_value" : "masterthesisonlineipaddress"
+        }
+      ]
+}
+
+module "jenkins" {
+  source  = "git::https://gitlab.hrz.tu-chemnitz.de/faeng--tu-chemnitz.de/jenkins_terraform_module.git"
+
+  credentials = var.credentials
+  host_name   = "jenkins.${var.root_domain}"
+}
+
+module "cert-manager" {
+  source  = "git::https://gitlab.hrz.tu-chemnitz.de/faeng--tu-chemnitz.de/terraform_cert_manger_google.git?ref=k8s-provider-integration"
+  # https://gitlab.hrz.tu-chemnitz.de/faeng--tu-chemnitz.de/terraform_cert_manger_google.git?ref=k8s-provider-integration
+  cluster_name            = module.gke.cluster_name
+  root_domain             = var.root_domain
+  lets_encrypt_email      = var.email
+  project_id              = var.project_id
+  location                = var.location
+  iam_account             = var.iam_account
+  acme_server_url         = "https://acme-staging-v02.api.letsencrypt.org/directory"
+}
---
+++ variables.tf
@@ -0,0 +1,41 @@
+
+variable "credentials" {
+    type    = list(any)
+}
+
+variable "location" {
+    description = "Region for the Google Cloud Platform"
+    type        = string
+}
+
+variable "zone" {
+    description = "Zone for the Google Cloud Platform Region"
+    type        = string
+    default     = "a"
+}
+
+variable "root_domain" {
+    description = "root domain for everything"
+    type        = string
+    default     = "masterthesis.online"
+}
+
+variable "project_id" {
+    description = "ID of the Google Cloud Project"
+    type        = string
+}
+
+variable "credential_file_name" {
+  description   = "the name of the json file with the credentials for the project, should be locatet in the root module"
+  type          = string
+}
+
+variable "email" {
+  description   = "email address used for letsencrypt"
+  type          = string
+}
+
+variable "iam_account" {
+  description   = "the gcp iam account name"
+  type          = string
+}
